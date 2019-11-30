2f1afb1af38c08224131bffb3aa607756d65f47b
Alex Deiter <alex.deiter@gmail.com>
2019-12-01 00:35:34 +0300

    NEX-22167 Sync NexentaStor Cinder NFS/iSCSI drivers: Master => Queens

--- cinder/volume/drivers/nexenta/nexentaedge/iscsi.py
+++ cinder/volume/drivers/nexenta/nexentaedge/iscsi.py
@@ -1,5 +1,4 @@
-# Copyright 2015 Nexenta Systems, Inc.
-# All Rights Reserved.
+# Copyright 2019 Nexenta by DDN, Inc. All rights reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -51,13 +50,7 @@ class NexentaEdgeISCSIDriver(driver.ISCSIDriver):
         super(NexentaEdgeISCSIDriver, self).__init__(*args, **kwargs)
         if self.configuration:
             self.configuration.append_config_values(
-                options.NEXENTA_CONNECTION_OPTS)
-            self.configuration.append_config_values(
-                options.NEXENTA_ISCSI_OPTS)
-            self.configuration.append_config_values(
-                options.NEXENTA_DATASET_OPTS)
-            self.configuration.append_config_values(
-                options.NEXENTA_EDGE_OPTS)
+                options.NEXENTAEDGE_ISCSI_OPTS)
         if self.configuration.nexenta_rest_address:
             self.restapi_host = self.configuration.nexenta_rest_address
         else:
